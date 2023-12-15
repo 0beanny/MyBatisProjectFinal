52ccf6cfd9103051aa2706175bced6267d2a9385
Gogumalang <22000269@handong.ac.kr>
2023-12-15 18:45:53 +0900

    push~

--- src/main/java/com/example/board/BoardController.java
+++ src/main/java/com/example/board/BoardController.java
@@ -3,9 +3,7 @@ package com.example.board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
